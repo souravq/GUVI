e2859e40a3fa539a0a09baa4b16b5dd9191b0024
souravq <47376123+souravq@users.noreply.github.com>
2019-02-06 21:57:28 +0530

    Create Fifth2.c

---
+++ Fifth2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int N,Q,i;
+	scanf("%d %d",&N,&Q);
+	for(i=N+1;i<Q;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d ",i);
+		}
+	}
+	return 0;
+}
