3b357a04d3b36dda421de5109b21cb5f07311388
souravq <47376123+souravq@users.noreply.github.com>
2019-02-06 10:41:11 +0530

    Create Third13.c

---
+++ Third13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	char ch;
+	int i;
+	scanf("%c",&ch);
+	i=(int)(ch);
+	if((i>=65&&i<=90)||(i>=97&&i<=122))
+	{
+		ch=(char)i;
+		if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+		  printf("Vowel");
+		else
+		  printf("Consonent");
+	}
+	else
+	  printf("Invalid");
+	
+	return 0;
+}
